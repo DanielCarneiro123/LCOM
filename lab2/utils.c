1ce303f2f425592e53c968de241a8ad81756355b
Afonso Vaz Osorio <up202108700@edu.fe.up.pt>
2023-02-27 12:07:48 +0000

    add lab2 files

---
+++ lab2/utils.c
@@ -0,0 +1,24 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  if (lsb == NULL) return 1;
+  *lsb = val & 0xFF;
+  return 0;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+  if (msb == NULL) return 1;
+  *msb = val >> 8;
+  return 0;
+}
+
+int (util_sys_inb)(int port, uint8_t *value) {
+  if (value == NULL) return 1;
+  uint32_t big_value;
+  if (sys_inb(port, &big_value)) return 1;
+  printf("%d", big_value);
+  *value = big_value & 0xFF;
+  return 0;
+}
