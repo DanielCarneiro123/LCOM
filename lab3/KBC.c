420628893ae4a547c95af6b547cabd9fdbd074ec
Afonso Vaz Osorio <up202108700@up.pt>
2023-03-10 17:01:00 +0000

    finish KBC.c

--- lab3/KBC.c
+++ lab3/KBC.c
@@ -1,1 +1,37 @@
 #include "KBC.h"
+
+int read_kbc_status(uint8_t* status) {
+    return util_sys_inb(KBC_STATUS_REG, status);
+}
+
+int read_kbc_output(uint8_t port, uint8_t *output) {
+    int attempts = 10;
+    uint8_t status;
+    while (attempts) {
+        if (read_kbc_status(&status)) return 1;
+        
+        if ((status & FULL_OUT_BUFFER)) {
+            if (util_sys_inb(port, output)) return 1;
+            if (status & PARITY_ERROR) return 1;
+            if (status & TIMEOUT_ERROR) return 1;
+            return 0;
+        }
+        tickdelay(micros_to_ticks(20000));
+        attempts--;
+    }
+    return 1;
+}
+
+int write_KBC_command(uint8_t port, uint8_t commandByte) {
+    int attempts = 10;
+    uint8_t status;
+    while (attempts) {
+        if (read_kbc_status(&status)) return 1;
+        if (!(status & FULL_IN_BUFFER)) {
+            return util_sys_outb(port, commandByte);
+        }
+        tickdelay(micros_to_ticks(20000));
+        attempts--;
+    }
+    return 1;
+}
--- lab3/keyboard.c
+++ lab3/keyboard.c
@@ -1,1 +1,20 @@
 #include "keyboad.h"
+
+int hook_id = 1;
+uint8_t scancode;
+
+int keyboard_subscribe_interrupts(uint8_t *bit_no) {
+  if (bit_no == NULL) return 1;
+  *bit_no = BIT(hook_id);
+  if (sys_irqsetpolicy(IRQ_KEYBOARD, IRQ_REENABLE | IRQ_EXCLUSIVE, &hook_id)) return 1;
+  return 0;
+}
+
+int keyboard_unsubscribe_interrupts() {
+  if (sys_irqrmpolicy(&hook_id)) return 1;
+  return 0;
+}
+
+void kbc_ih() {
+
+}
