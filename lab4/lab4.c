bc75dae2b0687097cb23f47a3c50d83650e418d0
Afonso Vaz Osorio <up202108700@up.pt>
2023-03-24 18:03:24 +0000

    finish lab4.2 hopefully

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -10,6 +10,7 @@
 
 extern bool error;
 extern uint8_t mouse_byte;
+extern int counter;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -101,9 +102,77 @@ int (mouse_test_packet)(uint32_t cnt) {
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, idle_time);
-    return 1;
+    //if (write_to_mouse(ENABLE_STREAM)) return 1;
+  if (mouse_enable_data_reporting()) return 1; //FAZER A MINHA PRÓPRIA FUNÇÃO QUE FAZ ISTO
+  uint8_t mask, mask_timer;
+  if (mouse_subscribe_interrupts(&mask)) return 1;
+  if (timer_subscribe_int(&mask_timer)) return 1;
+  uint32_t hz = sys_hz();
+  
+  int ipc_status;
+  message msg;
+  int r;
+  uint8_t curr_seconds = 0;
+  uint8_t curr = 0;
+  uint8_t curr_delay = 0;
+  struct packet mouse_packet;
+  int16_t delta_x, delta_y;
+
+  while(curr_seconds < idle_time) { /* You may want to use a different condition */
+    /* Get a request message. */
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+        printf("driver_receive failed with: %d", r);
+        continue;
+    }
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+        switch (_ENDPOINT_P(msg.m_source)) {
+            case HARDWARE: /* hardware interrupt notification */				
+                if (msg.m_notify.interrupts & mask) { /* subscribed interrupt */
+                  mouse_ih();
+                  if ((curr == 0) && !(BIT(3) & mouse_byte)) return 1;
+                  mouse_packet.bytes[curr] = mouse_byte;
+                  if (curr == 0) {
+                    mouse_packet.lb = mouse_byte & BIT(0);
+                    mouse_packet.rb = mouse_byte & BIT(1);
+                    mouse_packet.mb = mouse_byte & BIT(2);
+                    mouse_packet.x_ov = mouse_byte & BIT(6);
+                    mouse_packet.y_ov = mouse_byte & BIT(7);
+                    if (mouse_byte & BIT(4)) delta_x = 0xFF00;
+                    else delta_x = 0;
+                    if (mouse_byte & BIT(5)) delta_y = 0xFF00;
+                    else delta_y = 0;
+                  }
+                  else if (curr == 1) {
+                    delta_x |= mouse_byte;
+                    mouse_packet.delta_x = delta_x;
+                  }
+                  else if (curr == 2) {
+                    delta_y |= mouse_byte;
+                    mouse_packet.delta_y = delta_y;
+                    mouse_print_packet(&mouse_packet);
+                  }
+                  curr_seconds = 0;
+                  curr_delay = counter % hz;
+                  curr = (curr+1) % 3;
+                }
+                if (msg.m_notify.interrupts & mask_timer) {
+                  timer_int_handler();
+                  if (counter % hz == curr_delay) {
+                    curr_seconds++;
+                  }
+                }
+                break;
+            default:
+                break; /* no other notifications expected: do nothing */	
+        }
+    } else { /* received a standard message, not a notification */
+        /* no standard messages expected: do nothing */
+    }
+ }
+
+  if (mouse_unsubscribe_interrupts()) return 1;
+  if (mouse_restore()) return 1; 
+  return 0;
 }
 
 int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
---
+++ lab4/timer.c
@@ -0,0 +1,86 @@
+#include <lcom/lcf.h>
+#include <lcom/timer.h>
+
+#include <stdint.h>
+
+#include "i8254.h"
+
+int hook_id_timer = 0;
+int counter = 0;
+
+int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
+  if (timer != 0 && timer != 1 && timer != 2) return 1;
+  if (freq < 19 || freq > TIMER_FREQ) return 1;
+  uint16_t counter = TIMER_FREQ/freq;
+  uint8_t lsb, msb;
+  util_get_MSB(counter, &msb);
+  util_get_LSB(counter, &lsb);
+  uint8_t st;
+  if (timer_get_conf(timer, &st)) return 1;
+  st &= 0xF;
+  st = st | BIT(4) | BIT(5);
+  switch (timer) {
+    case 1:
+      st |= BIT(6);
+      break;
+    case 2:
+      st |= BIT(7);
+      break;
+  }
+  sys_outb(TIMER_CTRL, st);
+  sys_outb(0x40 + timer, lsb);
+  sys_outb(0x40 + timer, msb);
+  return 0;
+}
+
+int (timer_subscribe_int)(uint8_t *bit_no) {
+  if (bit_no == NULL) return 1;
+  *bit_no = BIT(hook_id_timer);
+  if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id_timer)) return 1;
+  return 0;
+}
+
+int (timer_unsubscribe_int)() {
+  if (sys_irqrmpolicy(&hook_id_timer)) return 1;
+  return 0;
+}
+
+void (timer_int_handler)() {
+  counter++;
+}
+
+int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+  if (timer > 2) return 1;
+  uint8_t command = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+  sys_outb(TIMER_CTRL, command);
+  if (util_sys_inb(0x40 + timer, st)) return 1;
+  return 0;
+}
+
+int (timer_display_conf)(uint8_t timer, uint8_t st,
+                        enum timer_status_field field) {
+  union timer_status_field_val conf;
+  switch(field) {
+    case tsf_all:
+      conf.byte = st;
+      break;
+    case tsf_initial:
+      st &= 0x30;
+      st >>= 4;
+      conf.in_mode = st;
+      break;
+    case tsf_mode:
+      st &= 0x0E;
+      st >>= 1;
+      if (st == 6) conf.count_mode = 2;
+      else if (st == 7) conf.count_mode = 3;
+      else conf.count_mode = st;
+      break;
+    case tsf_base:
+      st &= 0x01;
+      conf.bcd = (st == 1) ? true : false;
+      break; 
+  }
+  timer_print_config(timer, field, conf);
+  return 0;
+}
