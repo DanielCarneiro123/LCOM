2c84380f922203d53bf74de150f6e08830e0e95a
Onso37 <80353484+Onso37@users.noreply.github.com>
2023-05-11 12:55:10 +0100

    add parentheses

--- proj/src/controller/serialport/sp.c
+++ proj/src/controller/serialport/sp.c
@@ -3,29 +3,29 @@
 int hook_id_sp = 6;
 uint8_t sp_data = 0;
 
-int sp_setup() {
+int (sp_setup)() {
     if (sys_outb(COM2_BASE + LINE_CONTROL_OFFSET, BIT(0) | BIT(1))) return 1;
     if (sys_outb(COM2_BASE + INTERRUPT_ENABLE_OFFSET, BIT(0) | BIT(1))) return 1;
     return 0;
 }
 
-int sp_subscribe_interrupts(uint8_t *bit_no) {
+int (sp_subscribe_interrupts)(uint8_t *bit_no) {
   if (bit_no == NULL) return 1;
   *bit_no = BIT(hook_id_sp);
   if (sys_irqsetpolicy(COM2_IRQ, IRQ_REENABLE, &hook_id_sp)) return 1;
   return 0;
 }
 
-int sp_unsubscribe_interrupts() {
+int (sp_unsubscribe_interrupts)() {
   if (sys_irqrmpolicy(&hook_id_sp)) return 1;
   return 0;
 }
 
-int read_lsr(uint8_t *lsr) {
+int (read_lsr)(uint8_t *lsr) {
     return util_sys_inb(COM2_BASE + LINE_STATUS_OFFSET, lsr);
 }
 
-int read_sp_data() {
+int (read_sp_data)() {
     int attempts = SP_ATTEMPTS;
     uint8_t status;
     while (attempts) {
@@ -47,7 +47,7 @@ int read_sp_data() {
     return 1;
 }
 
-int write_sp_data(uint8_t data) {
+int (write_sp_data)(uint8_t data) {
     int attempts = SP_ATTEMPTS;
     uint8_t status;
     while (attempts) {
@@ -64,7 +64,7 @@ int write_sp_data(uint8_t data) {
     return 1;
 }
 
-void sp_ih() {
+void (sp_ih)() {
     uint8_t iir;
     util_sys_inb(COM2_BASE + INTERRUPT_IDENT_OFFSET, &iir);
     if ((iir & IIR_NO_PENDING) == 0) {
--- proj/src/controller/serialport/sp.h
+++ proj/src/controller/serialport/sp.h
@@ -5,12 +5,12 @@
 #include <lcom/lcf.h>
 #include "UART.h"
 
-int sp_setup();
-int sp_subscribe_interrupts(uint8_t *bit_no);
-int sp_unsubscribe_interrupts();
-void sp_ih();
-int read_sp_data();
-int read_lsr(uint8_t *lsr);
-int write_sp_data(uint8_t data);
+int (sp_setup)();
+int (sp_subscribe_interrupts)(uint8_t *bit_no);
+int (sp_unsubscribe_interrupts)();
+void (sp_ih)();
+int (read_sp_data)();
+int (read_lsr)(uint8_t *lsr);
+int (write_sp_data)(uint8_t data);
 
 #endif
