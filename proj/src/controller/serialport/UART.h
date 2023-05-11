f925a5e36360da6abe7ee5f4ea508c8dad1e33c2
Onso37 <80353484+Onso37@users.noreply.github.com>
2023-05-11 09:53:30 +0100

    start creating macros for sp

---
+++ proj/src/controller/serialport/UART.h
@@ -0,0 +1,16 @@
+#define COM1_BASE       0x3F8
+#define COM2_BASE       0x2F8
+
+#define COM1_IRQ        4
+#define COM2_IRQ        3
+
+#define RECEIVER_BUFFER_OFFSET          0
+#define TRANSMITTER_HOLDING_OFFSET      0
+#define INTERRUPT_ENABLE_OFFSET         1
+#define INTERRUPT_IDENT_OFFSET          2
+#define FIFO_CONTROL_OFFSET             2
+#define LINE_CONTROL_OFFSET             3
+#define MODEM_CONTROL_OFFSET            4
+#define LINE_STATUS_OFFSET              5
+#define MODEM_STATUS_OFFSET             6
+#define SCRATCHPAD_OFFSET               7
