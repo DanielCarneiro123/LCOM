5767ca36d72af668246aa6cac942a1f6ab52b8cb
Onso37 <80353484+Onso37@users.noreply.github.com>
2023-05-15 18:08:12 +0100

    make interrupts exclusive

--- proj/src/controller/serialport/sp.c
+++ proj/src/controller/serialport/sp.c
@@ -12,7 +12,7 @@ int (sp_setup)() {
 int (sp_subscribe_interrupts)(uint8_t *bit_no) {
   if (bit_no == NULL) return 1;
   *bit_no = BIT(hook_id_sp);
-  if (sys_irqsetpolicy(COM2_IRQ, IRQ_REENABLE, &hook_id_sp)) return 1;
+  if (sys_irqsetpolicy(COM2_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &hook_id_sp)) return 1;
   return 0;
 }
 
