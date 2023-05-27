4128b8cbff073e13a4e4084fcf406661066c21ef
Onso37 <afonso.vo@gmail.com>
2023-05-27 20:18:22 +0100

    doxygen for utils

--- proj/src/driver/utils.c
+++ proj/src/driver/utils.c
@@ -1,25 +1,41 @@
 #include <lcom/lcf.h>
 #include <stdint.h>
 
-// LSB -> Less Significant Bits
-int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  if (lsb == NULL) return 1; // o apontador deve ser válido
-  *lsb = 0xFF & val;         // preserva os 8 bits menos significativos
+/**
+ * @brief Gets the LSB from a byte
+ * @param val Value to extract LSB from
+ * @param lsb Resulting LSB
+ * @return int 1 on failure, 0 otherwise
+ */
+int (util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  if (lsb == NULL) return 1;
+  *lsb = val & 0xFF;
   return 0;
 }
 
-// MSB -> Most Significant Bits
-int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+/**
+ * @brief Gets the MSB from a byte
+ * @param val Value to extract MSB from
+ * @param msb Resulting MSB
+ * @return int 1 on failure, 0 otherwise
+ */
+int (util_get_MSB)(uint16_t val, uint8_t *msb) {
   if (msb == NULL) return 1;
   *msb = val >> 8;
   return 0;
 }
 
-// Transform 32 bit output in 8 bit output. Error prevention.
-int (util_sys_inb)(int port, uint8_t *value) {
-  if (value == NULL) return 1;    // o apontador deve ser válido
-  uint32_t val;                   // variável auxiliar de 32 bits
-  int ret = sys_inb(port, &val);  // val ficará com o valor lido de port
-  *value = 0xFF & val;            // value ficará apenas com os primeiros 8 bits do resultado
-  return ret;
+/**
+ * @brief Wrapper to sys_inb
+ * Allows sys_inb to be used with a uint8_t value instead of uint32_t
+ * @param port Port to read
+ * @param value Byte in which to store value
+ * @return int 1 on failure, 0 otherwise
+ */
+int util_sys_inb(int port, uint8_t *value) {
+  if (value == NULL) return 1;
+  uint32_t big_value;
+  if (sys_inb(port, &big_value)) return 1;
+  *value = big_value & 0xFF;
+  return 0;
 }
