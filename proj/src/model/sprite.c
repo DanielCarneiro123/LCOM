a5e8dbb155c33617085ec76267baa2793adc6592
Onso37 <afonso.vo@gmail.com>
2023-05-28 16:11:50 +0100

    remove unused fields

--- proj/src/model/sprite.c
+++ proj/src/model/sprite.c
@@ -11,17 +11,18 @@ extern MouseInfo mouse_info;
 Sprite *create_sprite_xpm(xpm_map_t sprite){
 
   Sprite *sp = (Sprite *) malloc (sizeof(Sprite));
-  if( sp == NULL ) return NULL;
+  if(sp == NULL) return NULL;
 
   xpm_image_t img;
   sp->colors = (uint32_t *) xpm_load(sprite, XPM_8_8_8_8, &img);
-  sp->height = img.height;
-  sp->width = img.width;
 
-  if( sp->colors == NULL ) {
+  if(sp->colors == NULL) {
     free(sp);
     return NULL;
   }
+  sp->height = img.height;
+  sp->width = img.width;
+
   return sp;
 }
 
--- proj/src/model/sprite.h
+++ proj/src/model/sprite.h
@@ -8,8 +8,6 @@ typedef struct {
     uint16_t height;
     uint16_t width;
     uint32_t *colors;
-    uint32_t color;
-    uint8_t pressed;
     int32_t x;
     int32_t y;
 } Sprite; 
